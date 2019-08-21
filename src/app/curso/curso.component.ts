a5c4bc0015f6b4c0c69ed890f2550a082bb84807
lucas-puff <lucas_puff@estudante.sc.senai.br>
2019-08-20 21:45:56 -0300

    formulario

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '../../../node_modules/@angular/forms';
+import { IfStmt } from '../../../node_modules/@angular/compiler';
+
 
 @Component({
   selector: 'app-curso',
@@ -6,13 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curso.component.css']
 })
 export class CursoComponent implements OnInit {
-
+  
   cursoAtual: string = "";
+  
+  curso: any ={
+    nome: null,
+    descricao: null,
+    email: null
+  }
+
 
   constructor() { }
 
   ngOnInit() {
     
+  }
+
+  onSubmit(formulario :NgForm){
+    if(formulario.valid){ 
+      console.log(formulario);
+      console.log('---valores do formulário');
+      console.log(formulario.form.value);
+      console.log('valor do objeto')
+      console.log(this.curso);}
+   
   }
 
  buscaAlunos(curso:string){
